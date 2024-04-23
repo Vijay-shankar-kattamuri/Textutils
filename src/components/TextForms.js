0ee6f0a3d0ddf602ac3e153bc938fb68400d0e8f
Kattamuri Vijay Shankar <vijayshankarkattamuri@gmail.com>
2024-04-24 01:47:38 +0530

    First commit

--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -23,6 +23,16 @@ export default function TextForms(props) {
         let newtext=text.replace(/\b\w/g, char => char.toUpperCase());
         settext(newtext)
       };
+      const copyToClipboard = () => {
+        var text =document.getElementById("myBox")
+        text.select();
+        navigator.clipboard.writeText(text.value)
+      }
+      const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        settext(newText.join(" "))
+      }
+      
     
     
     
@@ -39,18 +49,24 @@ export default function TextForms(props) {
      <h1>{props.heading}</h1>
 <div className="mb-3"  >
 
-  <textarea className="form-control" id="exampleFormControlTextarea1" style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} rows="8" onChange={handleOnClick}value={text}></textarea>
+  <textarea className="form-control" id="myBox" style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} rows="8" onChange={handleOnClick}value={text}></textarea>
 </div>
     </div>
     <button className="btn btn-primary mx-2 my-3" onClick={handleUpClick}>change to uppercase</button>
     <button className="btn btn-primary mx-2" onClick={handleLoClick}>change to lowercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Space</button>
     <button className="btn btn-primary mx-2" onClick={clear}>clear text</button>
+    <button className="btn btn-primary mx-2" onClick={copyToClipboard}>copy text</button>
+   
     
     <button className="btn btn-primary" onClick={capitalizeEveryWord}>capitalize</button>
     <div className="container" style={{color:props.mode==='dark'?'white':'black'}} >
         <h2>Text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} letters</p>
-        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <p>{text ? text.split(" ").length : 0} words and {text.length} letters</p>
+
+        <p>{text ? 0.008 * text.split(" ").length : 0} minutes read</p>
+
+
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter text here to show preview"}</p>
 
